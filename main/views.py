e6849a5b42eb6fb421ec919825ff43a590772ab6
James Frederix Rolianto <james.frederix@ui.ac.id>
2022-06-09 14:18:59 +0700

    remove unused variable

--- main/views.py
+++ main/views.py
@@ -17,7 +17,7 @@ def update(request):
         return JsonResponse(response)
 
 @api_view(['GET'])
-def read(request, npm, trx_id=None):
+def read(request, npm):
     try:
         mahasiswa = Mahasiswa.objects.get(npm=npm)
         response = {'status': 'OK', 'npm': mahasiswa.npm, 'nama': mahasiswa.nama}
